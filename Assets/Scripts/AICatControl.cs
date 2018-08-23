e07b8a12fa70651a666a702a18f452d2c73f9d49
DarkGraf <s_t2005@mail.ru>
2018-08-23 22:45:30 +0300

    Одиночное движение кота до цели.

---
+++ Assets/Scripts/AICatControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AICatControl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (agent == null)
+        {
+            return;
+        }
+
+        agent.SetDestination(target.transform.position);
+    }
+}
