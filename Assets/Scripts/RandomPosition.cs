8c9f3f481cc1f4dbc4db3df660c2c4326a8d6565
DarkGraf <s_t2005@mail.ru>
2018-08-24 21:58:58 +0300

    Добавлен SkyBox. Кот бегает за случайной целью.

---
+++ Assets/Scripts/RandomPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(RePositionWithDelay());
+    }
+
+    IEnumerator RePositionWithDelay()
+    {
+        while (true)
+        {
+            SetRandomPosition();
+            yield return new WaitForSeconds(20);
+        }
+    }
+
+    void SetRandomPosition()
+    {
+        float x = Random.Range(8, 42);
+        float z = Random.Range(8, 42);
+        Debug.Log("x, z = " + x.ToString("F2") + ", " + z.ToString("F2"));
+        transform.position = new Vector3(x, 0, z);
+    }
+}
