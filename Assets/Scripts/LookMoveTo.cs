42850252c7082e78469877406881ce8122389e94
DarkGraf <s_t2005@mail.ru>
2018-08-27 22:40:34 +0300

    Кот следует за взглядом.

---
+++ Assets/Scripts/LookMoveTo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookMoveTo : MonoBehaviour
+{
+    public GameObject ground;
+
+    void Update()
+    {
+        Transform camera = Camera.main.transform;
+
+        Debug.DrawRay(camera.position, camera.rotation * Vector3.forward * 100);
+
+        Ray ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject == ground)
+            {
+                Debug.Log("Hit (x, y, z): " + hit.point.ToString("F2"));
+                transform.position = hit.point;
+            }
+        }
+    }
+}
