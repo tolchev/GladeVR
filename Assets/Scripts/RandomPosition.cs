b045c88292ad121755e4f0b850d1d8897e790a21
DarkGraf <s_t2005@mail.ru>
2018-08-31 22:46:35 +0300

    Увеличено яблоко, уменьшен его радиус появления, изменен LOD.

--- Assets/Scripts/RandomPosition.cs
+++ Assets/Scripts/RandomPosition.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RandomPosition : MonoBehaviour
 {
     public GameObject centerObject;
-    public float radius = 10;
+    public float radius = 5;
 
     void Start()
     {
