ec7fb339406adb20ec8832f019c3002efda12f34
DarkGraf <s_t2005@mail.ru>
2018-09-11 22:52:55 +0300

    Координаты-выноски.

--- Assets/Scripts/LookMoveTo.cs
+++ Assets/Scripts/LookMoveTo.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LookMoveTo : MonoBehaviour
 {
     public GameObject ground;
+    public Transform infoBubble;
+    private Text infoText;
+
+    void Start()
+    {
+        if (infoBubble != null)
+        {
+            infoText = infoBubble.Find("Text").GetComponent<Text>();
+        }
+    }
 
     void Update()
     {
@@ -17,6 +28,14 @@ public class LookMoveTo : MonoBehaviour
             GameObject hitObject = hit.collider.gameObject;
             if (hitObject == ground)
             {
+                if (infoBubble != null)
+                {
+                    infoText.text = string.Format("X: {0}, Z: {1}", hit.point.x.ToString("F2"), hit.point.z.ToString("F2"));
+                    infoBubble.LookAt(camera.position);
+                    // После LookAt() сообщение будет развернуто в направлении от нас.
+                    infoBubble.Rotate(0, 180, 0);
+                }
+
                 transform.position = hit.point;
             }
         }
