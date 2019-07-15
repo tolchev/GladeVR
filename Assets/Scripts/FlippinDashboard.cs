b9bf3c3c98ff6231b285817426a30a97ff68b65c
DarkGraf <s_t2005@mail.ru>
2019-07-15 21:29:43 +0300

    Скрытие приборной панели.

---
+++ Assets/Scripts/FlippinDashboard.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class FlippinDashboard : MonoBehaviour 
+{
+    HeadGesture gesture;
+    GameObject dashboard;
+    bool isOpen = true;
+    Vector3 startRotation;
+
+    #region MonoBehaviour
+
+    void Start () 
+    {
+        gesture = GetComponent<HeadGesture>();
+        dashboard = GameObject.Find("Dashboard");
+        startRotation = dashboard.transform.eulerAngles;
+        CloseDashboard();
+    }
+	
+    void Update () 
+    {
+        if (gesture.isFacingDown)
+        {
+            OpenDashboard();
+        }
+        else
+        {
+            CloseDashboard();
+        }
+    }
+	
+    #endregion
+
+    private void CloseDashboard()
+    {
+        if (isOpen)
+        {
+            dashboard.transform.eulerAngles = new Vector3(180, startRotation.y, startRotation.z);
+            isOpen = false;
+        }
+    }
+
+    private void OpenDashboard()
+    {
+        if (!isOpen)
+        {
+            dashboard.transform.eulerAngles = startRotation;
+            isOpen = true;
+        }
+    }
+}
---
+++ Assets/Scripts/HeadGesture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HeadGesture : MonoBehaviour 
+{
+    public bool isFacingDown = false;
+
+    #region MonoBehaviour
+
+    void Update () 
+    {
+        isFacingDown = DetectFacingDown();
+    }
+
+    #endregion
+
+    private bool DetectFacingDown()
+    {
+        return CameraAngleFromGround() < 60;
+    }
+
+    private float CameraAngleFromGround()
+    {
+        return Vector3.Angle(Vector3.down, Camera.main.transform.rotation * Vector3.forward);
+    }
+}
